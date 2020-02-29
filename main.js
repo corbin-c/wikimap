1d894d99d0004e48e778af1c85c24c291c157269
Clément Corbin <corbin.clement@gmail.com>
2020-02-29 14:00:03 +0100

    getting properties for a list of titles

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const FEED = ".wikipedia.org/w/api.php?action=feedrecentchanges&hidebots=1&hidecategorization=1&hideWikibase=1&limit=50&days=7&urlversion=2&feedformat=atom&namespace=0&origin=*&utf8";
+const GEO = ".wikipedia.org/w/api.php?action=query&format=json&origin=*&utf8&titles=";
 
 const getFeed = async (lang,date=false) => {
   let getId = (url) => url.split("&diff=")[1].split("&oldid")[0];
@@ -16,5 +17,21 @@ const getFeed = async (lang,date=false) => {
   };
   return feed;
 }
+let getProperties = async (lang,titles,types) => {
+  let properties = "https://"+lang+GEO+titles.join("|")+"&prop="+types.join("|");
+  properties = await fetch(properties);
+  properties = await properties.json();
+  return Object.keys(properties.query.pages).map(id => {
+    let output = { title:properties.query.pages[id].title };
+    types.map(e => {
+      if (Object.keys(properties.query.pages[id]).indexOf(e) >= 0) {
+        output[e] = properties.query.pages[id][e];
+      } else {
+        output[e] = false;
+      }
+    });
+    return output;
+  });
+};
 
 export { getFeed };
